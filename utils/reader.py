7ebc9bba0a4ee73a5e56f671c72d57a117f11ca0
Sofiia Yeremeieva <s.yeremeieva@gmail.com>
2024-03-19 15:28:05 -0400

    Started basic text transformation to graph

--- utils/reader.py
+++ utils/reader.py
@@ -1,5 +1,6 @@
 import PyPDF2
 import os
+from langchain.docstore.document import Document
 
 from utils.debugger import logger
 
@@ -38,7 +39,7 @@ def read_txt(path_pdf):
         logger.info(f"The folder '{path_pdf}' does not exist.")
         return
     try:
-        with open(path_pdf, "rb") as file:
+        with open(path_pdf, "r") as file:
             return file.read()
     except Exception as e_read:
         logger.exception(f'not successful read of txt, exception "{e_read}"')
@@ -47,3 +48,27 @@ def read_txt(path_pdf):
 def write_txt(path_txt, text):
     with open(path_txt, 'w') as file:
         file.writelines(text)
+
+
+# class Document:
+#     def __init__(self, page_content, metadata=None):
+#         self.page_content = page_content
+#         self.metadata = metadata if metadata is not None else {}
+
+def txt_to_doc(file_path):
+    text_content = read_txt(file_path)
+    metadata = {
+        'title': file_path[36:-4],
+        'summary': 'no',
+        'source': file_path,
+        'id': 1
+    }
+    return Document(text_content, metadata=metadata)
+
+# if __name__ == '__main__':
+#     doc = txt_to_doc('data/txt_parsed_papers/parsed_paper_innovations.txt')
+#
+#     text_splitter = TokenTextSplitter(chunk_size=2048, chunk_overlap=24)
+#
+#     documents = text_splitter.split_documents([doc])
+#     print(documents)
