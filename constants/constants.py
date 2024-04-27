baad297269158b6cd609e9fce561b2e9e2b00c30
Sofiia Yeremeieva <s.yeremeieva@gmail.com>
2024-04-27 16:02:53 -0400

    Adjusted PageRank

--- constants/constants.py
+++ constants/constants.py
@@ -9,27 +9,29 @@ QUERIES = {
                       RETURN length(path) AS diameter
                       ORDER BY diameter
                       DESC LIMIT 1""",
+
     'count_nodes': """MATCH (n)
                       RETURN count(n) as count_nodes""",
+
     'count_edges': """MATCH ()-[r]->()
                       RETURN count(r) as count_edges""",
+
     'isolated_nodes': """MATCH (n) 
                          WHERE NOT EXISTS ((n)<-[]-()) RETURN count(n) as isolated_nodes""",
+
     'degree_nodes': """MATCH (n)
                        WITH COUNT{(n)-[]-()} as degree
                        WHERE  degree >= 1
                        RETURN degree""",
-    'pagerank': """CALL gds.pageRank.stream('full_graph', {
-                        maxIterations: 20,
-                        dampingFactor: 0.85
-                   })
+
+    'pagerank': """CALL gds.pageRank.stream('full_graph')
                    YIELD nodeId, score
-                   RETURN gds.util.asNode(nodeId).id AS id,      
-                   gds.util.asNode(nodeId).name as name, score as full_pagerank,
-                   ORDER BY full_pagerank DESC""",
-    'create_graph': """CALL gds.graph.project.cypher('full_graph',
-                       MATCH (n) RETURN id(n) AS id,
-                       MATCH (n)-[e]-(m) RETURN id(n) AS source, id(m) AS target
+                   RETURN gds.util.asNode(nodeId).name AS name, score
+                   ORDER BY score DESC, name ASC""",
+
+    'create_full_graph': """CALL gds.graph.project.cypher('full_graph',
+                       'MATCH (n) RETURN id(n) AS id',
+                       'MATCH (n)-[e]-(m) RETURN id(n) AS source, id(m) AS target'
                        )"""
 
 }
