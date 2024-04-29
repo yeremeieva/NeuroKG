375be4e69aa14425ccad3762d5e967b02d61175f
Sofiia Yeremeieva <s.yeremeieva@gmail.com>
2024-04-29 12:20:49 -0400

    Finished working on adjacency_matrix

--- constants/constants.py
+++ constants/constants.py
@@ -33,14 +33,12 @@ QUERIES = {
                        'MATCH (n) RETURN id(n) AS id',
                        'MATCH (n)-[e]-(m) RETURN id(n) AS source, id(m) AS target'
                        )""",
-    'adjacency_matrix': """MATCH (n1)
-                           WITH collect(n1) as nodes1
-                           MATCH (n2)
-                           WITH nodes1, collect(n2) as nodes2
-                           UNWIND nodes1 as node1
-                           UNWIND nodes2 as node2
-                           OPTIONAL MATCH (node1)-[]->(node2)
-                           RETURN node1.id as node1_id, node2.id as node2_id""",
+
+    'adjacency_matrix': """MATCH (n) 
+                           MATCH (m)
+                           WHERE EXISTS ((n)-[]-(m))
+                           RETURN n.id as node1_id, m.id as node2_id""",
+
     'nodes_names': """MATCH (n) 
                       WHERE EXISTS ((n)-[]-())
                       RETURN n.id as node_id"""
