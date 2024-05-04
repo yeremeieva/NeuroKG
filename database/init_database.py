9592ab48bb4d11111ef818b483f8258b4f69d4e7
Sofiia Yeremeieva <s.yeremeieva@gmail.com>
2024-05-04 12:06:16 -0400

    Minor changes

--- database/init_database.py
+++ database/init_database.py
@@ -7,6 +7,7 @@ from langchain_community.graphs.graph_document import (Node as BaseNode, Relatio
 
 from utils.debugger import logger
 
+
 def init_knowledge_base() -> Neo4jGraph:
     try:
         load_dotenv()
@@ -22,18 +23,21 @@ def init_knowledge_base() -> Neo4jGraph:
 
 
 class Property(BaseModel):
-  """A single property consisting of key and value"""
-  key: str = Field(..., description="key")
-  value: Optional[str] = Field(..., description="value")
+    """A single property consisting of key and value"""
+    key: str = Field(..., description="key")
+    value: Optional[str] = Field(..., description="value")
+
 
 class Node(BaseNode):
     properties: Optional[List[Property]] = Field(
         None, description="List of node properties")
 
+
 class Relationship(BaseRelationship):
     properties: Optional[List[Property]] = Field(
         None, description="List of relationship properties"
     )
+
 
 class KnowledgeGraph(BaseModel):
     """Generate a knowledge graph with entities and relationships."""
